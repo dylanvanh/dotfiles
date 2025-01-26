46fc385f5c63519778129d30b076bb725491f524
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-01-26 19:49:18 +0200

    disable snacks.nvim smooth scroll enabled default

---
+++ nvim/lua/plugins/ui/snacks.lua
@@ -0,0 +1,14 @@
+-- snacks.nvim
+-- https://github.com/folke/snacks.nvim/blob/main/docs/scroll.md
+-- lazy.nvim
+return {
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = false,
+      -- or leave it empty to use the default settings
+      -- your scroll configuration comes here
+      -- refer to the configuration section below
+    },
+  },
+}
