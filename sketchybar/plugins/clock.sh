f50f353e338fc36878cc2cad719e3270c1966463
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-09-23 12:39:06 +0200

    add date to sketchybar clock plugin

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -4,7 +4,8 @@
 source "$CONFIG_DIR/colors.sh"
 
 # Get current time in 24-hour format
-LABEL=$(date "+%H:%M")
+LABEL=$(date "+%d/%m %H:%M")
 
 # Set the clock label
 sketchybar --set "$NAME" label="$LABEL"
+
